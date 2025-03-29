797148543ace726d1b9e41bff837d46f8344582d
ck <ck@gmail.com>
2025-03-30 04:17:59 +0800

    删除加密和解密

--- src/main/java/ck/infrastructure/InfrastructureRunner.java
+++ src/main/java/ck/infrastructure/InfrastructureRunner.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@OpenAPIDefinition(info = @Info(description = "所有的请求体必须AES加密，/aes/key/encrypt接口可以用来测试加密"))
 @SpringBootApplication
 public class InfrastructureRunner {
     public static void main(String[] args) {
